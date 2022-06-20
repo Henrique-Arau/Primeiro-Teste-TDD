c86981739ed567045ddae9dbff5e68eb8e6f148c
Henrique-Arau <penielh43@gmail.com>
2022-06-20 17:53:42 -0300

    cadastros de pessoas atualizadas

---
+++ src/main/java/org/example/PessoaSemNotException.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class PessoaSemNotException extends RuntimeException {
+}
--- src/test/java/org/example/CadastroPessoaTest.java
+++ src/test/java/org/example/CadastroPessoaTest.java
@@ -1,6 +1,71 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class CadastroPessoaTest {
 
-    
+    @Test
+    public void deveCriarOCadastroDePessoas() {
+        //cenario e execução
+        CadastroPessoas cadastro = new CadastroPessoas();
+
+        //verificação
+        Assertions.assertThat(cadastro.getPessoas()).isEmpty();
+    }
+    @Test
+    public void deveAdicionarUmaPessoa() {
+        //cenario
+        CadastroPessoas cadastroPessoas = new CadastroPessoas();
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Henrique");
+
+
+        //execução
+        cadastroPessoas.adicionar(pessoa);
+
+        //verificação
+        Assertions.assertThat(cadastroPessoas.getPessoas())
+                .isNotEmpty()
+                .hasSize(1)
+                .contains(pessoa);
+
+    }
+
+    @Test(expected = PessoaSemNotException.class)
+    public void naoDeveAdicionarPessoaComNomeVazio() {
+        //cenario
+        CadastroPessoas cadastroPessoas = new CadastroPessoas();
+        Pessoa pessoa = new Pessoa();
+
+        //execução
+        cadastroPessoas.adicionar(pessoa);
+    }
+
+    @Test
+    public void deveRemoverUmaPessoa() {
+        //cenario
+        CadastroPessoas cadastroPessoas = new CadastroPessoas();
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Henrique");
+        cadastroPessoas.adicionar(pessoa);
+
+        //execução
+        cadastroPessoas.remover(pessoa);
+
+        //verificação
+        Assertions.assertThat(cadastroPessoas.getPessoas()).isEmpty();
+
+
+    }
+    @Test(expected = CadastroVazioException.class)
+    public void deveLancarErroAoTentarRemoverPessoaInexistente() {
+        //cenario
+        CadastroPessoas cadastroPessoas = new CadastroPessoas();
+        Pessoa pessoa = new Pessoa();
+
+        //execução
+        cadastroPessoas.remover (pessoa);
+
+    }
 }
